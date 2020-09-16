20c2e994e71a590a58c06d41a0ed6ec478096a66
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2020-09-16 13:58:35 +0200

    Supporting Additional Formats

--- program.py
+++ program.py
@@ -1,8 +1,12 @@
 import songs
 import serializers
+import yaml_serializer
 
 song = songs.Song("1", "Water of Love", "Dire Straits")
 serializer = serializers.ObjectSerializer()
 
 print(serializer.serialize(song, "JSON"))
+print("")
 print(serializer.serialize(song, "XML"))
+print("")
+print(serializer.serialize(song, "YAML"))
--- serializers.py
+++ serializers.py
@@ -32,16 +32,22 @@ class XmlSerializer:
 
 
 class SerializerFactory:
+    def __init__(self) -> None:
+        self._creators = {}
+
+    def register_format(self, format, creator):
+        self._creators[format] = creator
+
     def get_serializer(self, format):
-        if format == "JSON":
-            return JsonSerializer()
-        elif format == "XML":
-            return XmlSerializer()
-        else:
+        creator = self._creators.get(format)
+        if not creator:
             raise ValueError(format)
+        return creator()
 
 
 factory = SerializerFactory()
+factory.register_format("JSON", JsonSerializer)
+factory.register_format("XML", XmlSerializer)
 
 
 class ObjectSerializer:
