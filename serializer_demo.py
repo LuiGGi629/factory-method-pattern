7821e95962fa753e004133049dfa02273844951f
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2020-09-16 10:37:13 +0200

    Basic Implementation of Factory Method

--- serializer_demo.py
+++ serializer_demo.py
@@ -11,21 +11,28 @@ class Song:
 
 class SongSerializer:
     def serialize(self, song, format):
-        if format == "JSON":
-            return self._serialize_to_json(song)
-        elif format == "XML":
-            return self._serialize_to_xml(song)
-        else:
-            raise ValueError(format)
+        serializer = get_serializer(format)
+        return serializer(song)
 
-    def _serialize_to_json(self, song):
-        payload = {"id": song.song_id, "title": song.title, "artist": song.artist}
-        return json.dumps(payload)
 
-    def _serialize_to_xml(self, song):
-        song_element = et.Element("song", attrib={"id": song.song_id})
-        title = et.SubElement(song_element, "title")
-        title.text = song.title
-        artist = et.SubElement(song_element, "artist")
-        artist.text = song.artist
-        return et.tostring(song_element, encoding="unicode")
+def get_serializer(format):
+    if format == "JSON":
+        return _serialize_to_json
+    elif format == "XML":
+        return _serialize_to_xml
+    else:
+        raise ValueError(format)
+
+
+def _serialize_to_json(song):
+    payload = {"id": song.song_id, "title": song.title, "artist": song.artist}
+    return json.dumps(payload)
+
+
+def _serialize_to_xml(song):
+    song_element = et.Element("song", attrib={"id": song.song_id})
+    title = et.SubElement(song_element, "title")
+    title.text = song.title
+    artist = et.SubElement(song_element, "artist")
+    artist.text = song.artist
+    return et.tostring(song_element, encoding="unicode")
